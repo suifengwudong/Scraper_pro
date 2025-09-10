4bc99f5f839d88e87e08fb67159a8f888d9a794d
suifengwudong <HCXCNC@outlook.com>
2025-09-10 11:51:32 +0800

    feat(dev): add configuration file, refine driver and get_html files`

--- src/scrape/get_html.py
+++ src/scrape/get_html.py
@@ -1,4 +1,16 @@
 import time
+import config
+
+driver = None  # save the driver instance here
+
+def get_driver_instance(browser_name: str = config.driver_used, driver_path: str = config.BROWSER_PATH_DICT[config.driver_used]):
+
+    global driver
+    if driver is None:
+        from driver import Driver
+        driver_manager = Driver(browser_name, driver_path)
+        driver = driver_manager.acquire()
+    return driver
 
 def get_html(url: str, option: str) -> str:
     '''Get HTML content from a webpage using the specified method.
@@ -10,32 +22,33 @@ def get_html(url: str, option: str) -> str:
     Raises:
         ValueError: If an unsupported option is provided.
     '''
+    html = ""
     match option.lower():
         case "requests":
             import requests
             headers = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
             }
-            return requests.get(url, headers=headers).text
+            html = requests.get(url, headers=headers).text
         case "selenium":
-            from driver import Driver
-            driver_manager = Driver("edge", "E:/PYTHON/Scripts/msedgedriver.exe")
-            instance = driver_manager.acquire()
+            driver_name = config.driver_used
+            instance = get_driver_instance(driver_name, config.BROWSER_PATH_DICT[driver_name])  # or "firefox", "edge"
             if instance is None:
                 raise RuntimeError("WebDriver failed to initialize.")
-            try:
-                instance.get(url)
-                time.sleep(3)
-                return instance.page_source
-            finally:
-                Driver.quit()  # TODO: quit太草率了，应添加更多的逻辑（或许异步？）
+            instance.get(url)
+            time.sleep(3)
+            html = instance.page_source
         case _:
             raise ValueError(f"Unsupported option: {option}")
+    
+    if (html == ""):
+        raise RuntimeError("Failed to retrieve HTML content.")
+    return html
 
 
 if __name__ == "__main__":
     url = "https://movie.douban.com/"
     option = "requests"
     html = get_html(url, option)
-    with open("output2.html", "w", encoding="utf-8") as f:
+    with open("src/test/data/output2.html", "w", encoding="utf-8") as f:
         f.write(html)
